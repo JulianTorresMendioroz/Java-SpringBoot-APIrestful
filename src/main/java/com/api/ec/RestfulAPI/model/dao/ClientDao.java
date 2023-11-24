fd4ab788b43927c576690c9c150206b1f6fd31b2
Julian Torres Mendioroz <juliantorresmendioroz348@gmail.com>
2023-11-24 19:05:36 -0300

    cambio a crud repository de DAO

--- src/main/java/com/api/ec/RestfulAPI/model/dao/ClientDao.java
+++ src/main/java/com/api/ec/RestfulAPI/model/dao/ClientDao.java
@@ -1,9 +1,8 @@
 package com.api.ec.RestfulAPI.model.dao;
 
 import com.api.ec.RestfulAPI.model.entity.Client;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
+import org.springframework.data.repository.CrudRepository;
 //heredo clase para CRUD
-public interface ClientDao extends PagingAndSortingRepository<Client, Integer> {
+public interface ClientDao extends CrudRepository<Client, Integer> {
 
 }
